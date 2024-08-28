4325377e49d3ce203701bf8f2a69bc4a11b88c9f
telpvn <telpvn@mail.ru>
2024-08-28 13:44:17 +0400

    Connecting a Microservice to PostgreSQL Using Spring Data JPA

---
+++ node/src/main/java/ru/relex/dao/RawDataDAO.java
@@ -0,0 +1,8 @@
+package ru.relex.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.relex.entity.RawData;
+
+public interface RawDataDAO extends JpaRepository<RawData, Long> {
+
+}
